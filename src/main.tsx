8bdfe140fdf01c774f047eeb05ff12456a03695d
Bettenoch <bettenoch254@gmail.com>
2024-12-02 11:45:06 +0300

    ADD: wrap main file with chakra provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
+import { ColorModeProvider } from "./components/ui/color-mode.tsx"
+import { Provider } from "./components/ui/provider.tsx"
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<Provider>
+			<App />
+		</Provider>
 	</StrictMode>,
 )
