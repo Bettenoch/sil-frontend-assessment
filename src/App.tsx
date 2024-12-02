178b38ce03b5993f59a79b5b4726ef3eb0b63efc
Bettenoch <bettenoch254@gmail.com>
2024-12-02 11:44:42 +0300

    TEST: page elements and light and dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from "react"
-import { useState } from "react"
 import "./App.css"
+import { Box, Container, Heading } from "@chakra-ui/react"
+import { ColorModeButton } from "./components/ui/color-mode"
 
 function App() {
-	const [count, setCount] = useState(0)
-
-	return <></>
+	return (
+		<>
+			<Container maxW={"100%"}>
+				<Box>
+					<Heading colorPalette={"brand"}>WELCOME HOME</Heading>
+				</Box>
+				<Box>
+					<ColorModeButton />
+				</Box>
+			</Container>
+		</>
+	)
 }
 
 export default App
