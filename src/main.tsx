3fecbdcac351b065b887313e0b1f8fad31635dce
Bettenoch <bettenoch254@gmail.com>
2024-12-02 12:17:30 +0300

    MODIFY: app entry file to use tanstack routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,26 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { RouterProvider, createRouter } from "@tanstack/react-router"
 import React from "react"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
-import App from "./App.tsx"
-import { ColorModeProvider } from "./components/ui/color-mode.tsx"
+import { routeTree } from "./routeTree.gen"
+
 import { Provider } from "./components/ui/provider.tsx"
 
+const queryClient = new QueryClient()
+const router = createRouter({ routeTree })
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router
+	}
+}
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<Provider>
-			<App />
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider router={router} />
+			</QueryClientProvider>
 		</Provider>
 	</StrictMode>,
 )
