9a4c5121aa02c494222943942078518ba78aeb97
Bettenoch <bettenoch254@gmail.com>
2024-12-04 00:06:26 +0300

    MODIFY: add position absolute

--- src/components/common/CustomFooter.tsx
+++ src/components/common/CustomFooter.tsx
@@ -1,38 +1,41 @@
-import { Flex } from "@chakra-ui/react"
-import React from "react"
-import { Button } from "../../components/ui/button"
+import { Flex } from "@chakra-ui/react";
+import { Button } from "../../components/ui/button";
 
 type PaginationFooterProps = {
-	hasNextPage?: boolean
-	hasPreviousPage?: boolean
-	onChangePage: (newPage: number) => void
-	page: number
-}
+  hasNextPage?: boolean;
+  hasPreviousPage?: boolean;
+  onChangePage: (newPage: number) => void;
+  page: number;
+};
 
 export function CustomFooter({
-	hasNextPage,
-	hasPreviousPage,
-	onChangePage,
-	page,
+  hasNextPage,
+  hasPreviousPage,
+  onChangePage,
+  page,
 }: PaginationFooterProps) {
-	return (
-		<Flex
-			gap={4}
-			alignItems="center"
-			mt={4}
-			direction="row"
-			justifyContent="flex-end"
-		>
-			<Button
-				onClick={() => onChangePage(page - 1)}
-				disabled={!hasPreviousPage || page <= 1}
-			>
-				Previous
-			</Button>
-			<span>Page {page}</span>
-			<Button disabled={!hasNextPage} onClick={() => onChangePage(page + 1)}>
-				Next
-			</Button>
-		</Flex>
-	)
+  return (
+    <Flex
+
+      position={"absolute"}
+      bottom={0}
+      w="full"
+      gap={4}
+      alignItems="center"
+      mt={8}
+      direction="row"
+      justifyContent="flex-end"
+    >
+      <Button
+        onClick={() => onChangePage(page - 1)}
+        disabled={!hasPreviousPage || page <= 1}
+      >
+        Previous
+      </Button>
+      <span>Page {page}</span>
+      <Button disabled={!hasNextPage} onClick={() => onChangePage(page + 1)}>
+        Next
+      </Button>
+    </Flex>
+  );
 }
