2b993e05d11d7343da649c87b8efd3d2ecb6e716
Bettenoch <bettenoch254@gmail.com>
2024-12-05 08:42:50 +0300

    ADD: route to album photos

--- src/routes/_layout/users/$userId/albums/index.tsx
+++ src/routes/_layout/users/$userId/albums/index.tsx
@@ -16,6 +16,7 @@ import { z } from "zod";
 import { useEffect } from "react";
 import { CustomFooter } from "../../../../../components/common/CustomFooter";
 import { LuArrowBigLeft } from "react-icons/lu";
+import {Route as AlbumPhotos} from "./$albumId/photos/"
 
 const albumSearchSchema = z.object({
   page: z.number().catch(1),
@@ -32,15 +33,20 @@ const total_albums_per_page = 20;
 
 function getUserAlbums({ userId, page }: { userId: string; page: number }) {
   return {
-    queryFn: () => UserAlbumsService.getAllUserAlbums({ userId: userId }),
+    queryFn: () =>
+      UserAlbumsService.getAllUserAlbums({
+        userId: userId,
+        skip: (page - 1) * total_albums_per_page,
+        limit: total_albums_per_page,
+      }),
     queryKey: ["user_albums", { userId, page }],
   };
 }
-function getUserById({userId}:{userId: string}) {
+export function getUserById({ userId }: { userId: string }) {
   return {
-    queryFn: () => UsersService.getUserById({userId:userId }),
-    queryKey: ["user", userId]
-  }
+    queryFn: () => UsersService.getUserById({ userId: userId }),
+    queryKey: ["user", userId],
+  };
 }
 function UserAlbum() {
   const navigate = useNavigate({ from: Route.fullPath });
@@ -53,7 +59,6 @@ function UserAlbum() {
       search: (prev: { [key: string]: string }) => ({ ...prev, page }),
     });
 
-
   const {
     data: albums,
     isPending,
@@ -61,7 +66,7 @@ function UserAlbum() {
   } = useQuery({
     ...getUserAlbums({ userId, page }),
   });
-  const count = albums?.data.length
+  const count = albums?.data.length;
 
   const hasNextPage =
     !isPlaceholderData && albums?.data.length === total_albums_per_page;
@@ -73,60 +78,107 @@ function UserAlbum() {
     }
   }, [page, queryClient, hasNextPage]);
 
-  const {data:user}= useQuery(getUserById({userId}))
+  const { data: user } = useQuery(getUserById({ userId }));
 
   return (
-    <Container maxW={"100%"} >
-      <Box w="full" p={6} mt={24} display={"flex"} minH={"100vh"} flexDirection={"column"} gap={12}>
-        <Flex w="full"  alignItems={"center"} gap={6} direction={{base:"column-reverse", md:"row"}} justifyContent={"space-around"}>
+    <Container maxW={"100%"}>
+      <Box
+        w="full"
+        p={6}
+        mt={24}
+        display={"flex"}
+        minH={"100vh"}
+        flexDirection={"column"}
+        gap={12}
+      >
+        <Flex
+          w="full"
+          alignItems={"center"}
+          gap={6}
+          direction={{ base: "column-reverse", md: "row" }}
+          justifyContent={"space-around"}
+        >
           <Box className="">
-            {
-              user && (
-                <Flex direction={"column"} gap={4}>
-                  <Text fontSize={"2xl"} fontWeight={"semibold"}>{count} Albums</Text>
-                  <Link to={"/users"} className="flex">
+            {user && (
+              <Flex direction={"column"} gap={4}>
+                <Text fontSize={"2xl"} fontWeight={"semibold"}>
+                  {count} Albums
+                </Text>
+                <Link to={"/users"} className="flex">
                   <Flex>
-                  <LuArrowBigLeft/>
-                  <Text textDecoration={"underline"}>Back to home</Text>
+                    <LuArrowBigLeft />
+                    <Text textDecoration={"underline"}>Back to home</Text>
                   </Flex>
-                  </Link>
-                </Flex>
-              )
-            }
-
+                </Link>
+              </Flex>
+            )}
           </Box>
-         <Flex border={"1px solid"} borderRadius={"2em"} alignItems={"center"} direction={"column"} >
-         <Box p={4}>
-         {
-            user && (
-              <Box>
-                 <Image
-                        src={user.avatar || ""}
-                        alt={user.username}
-                        w={24}
-                        h={24}
-                        rounded={"full"}
-                      />
+          <Flex
+            border={"1px solid"}
+            borderRadius={"2em"}
+            alignItems={"center"}
+            direction={"column"}
+          >
+            <Box p={4}>
+              {user && (
+                <Box>
+                  <Image
+                    src={user.avatar || ""}
+                    alt={user.username}
+                    w={24}
+                    h={24}
+                    rounded={"full"}
+                  />
+                </Box>
+              )}
+            </Box>
+            <VStack p={4}>
+              <Box
+                w="full"
+                display={"flex"}
+                alignItems={"center"}
+                justifyContent={"end"}
+              >
+                <Text>{}</Text>
               </Box>
-            )
-          }
-         </Box>
-          <VStack p={4} >
-            <Box w="full" display={"flex"} alignItems={"center"} justifyContent={"end"}>
-              <Text>{}</Text>
-            </Box>
-            <Box display={"flex"} flexDir={"column"} alignItems={"flex-start"}>
-              <Text fontSize={"4xl"} fontWeight={"bold"}>{user?.name}</Text>
-              <Text fontSize={"md"} fontWeight={"light"} fontStyle={"italic"}>USERNAME: {user?.username}</Text>
-              <Text fontSize={"sm"} fontWeight={"semibold"} textDecoration={"underline"}>EMAIL: {user?.email}</Text>
-            </Box>
-
-          </VStack>
-         </Flex>
+              <Box
+                display={"flex"}
+                flexDir={"column"}
+                alignItems={"flex-start"}
+              >
+                <Text fontSize={"4xl"} fontWeight={"bold"}>
+                  {user?.name}
+                </Text>
+                <Text fontSize={"md"} fontWeight={"light"} fontStyle={"italic"}>
+                  USERNAME: {user?.username}
+                </Text>
+                <Text
+                  fontSize={"sm"}
+                  fontWeight={"semibold"}
+                  textDecoration={"underline"}
+                >
+                  EMAIL: {user?.email}
+                </Text>
+              </Box>
+            </VStack>
+          </Flex>
         </Flex>
-        <Flex w="full" position={"relative"} minH={"80vh"} direction={"column"} gap={4}>
-          <Box w={"full"} alignItems={"center"} justifyContent={"center"} bg={"bg.panel"}>
-            <Text fontWeight={"bold"} fontSize={"5xl"} >Albums</Text>
+        <Flex
+          w="full"
+          position={"relative"}
+          minH={"80vh"}
+          direction={"column"}
+          gap={4}
+        >
+          <Box
+            w={"full"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            bg={"bg.panel"}
+          >
+            <Text fontWeight={"bold"} fontSize={"5xl"}>
+              Albums
+            </Text>
           </Box>
           <Grid
             w="full"
@@ -150,16 +202,58 @@ function UserAlbum() {
             ) : (
               <>
                 {albums?.data.map((album) => (
-                 <GridItem key={album.id} display={"flex"} w="full" flexDir={"column"} >
-                  <Box borderTopStartRadius={"3em"} w={"full"} h={64} position={"relative"} display={"flex"} bg={`url(${album.cover_photo || "https://images.unsplash.com/photo-1517770413964-df8ca61194a6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJvb2t8ZW58MHx8MHx8fDA%3D"})`} backgroundPosition={"center"} backgroundSize={"cover"} backgroundRepeat={"no-repeat"}>
-                    <Box position={"absolute"}  bottom={0} display={"flex"} w="full" flexDirection={"column"} alignItems={"center"} justifyContent={"center"}>
-                      <Box position={"relative"} display={"flex"} maxW={"max-content"}>
-                        <Box position={"absolute"} inset={0} bg={"black"} opacity={0.6}/>
-                      <Text position={"relative"} px={2} color={"#F5F5F5"} fontSize={"2xl"} fontWeight={"semibold"}>{album.title}</Text>
+                  <GridItem
+                    key={album.id}
+                    display={"flex"}
+                    w="full"
+                    flexDir={"column"}
+                  >
+                 <Link to={AlbumPhotos.fullPath} params={{ userId: album.owner_id, albumId:album.id }}>
+                 <Box
+                      borderTopStartRadius={"3em"}
+                      w={"full"}
+                      h={64}
+                      position={"relative"}
+                      display={"flex"}
+                      bg={`url(${album.cover_photo || "https://images.unsplash.com/photo-1517770413964-df8ca61194a6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJvb2t8ZW58MHx8MHx8fDA%3D"})`}
+                      backgroundPosition={"center"}
+                      backgroundSize={"cover"}
+                      backgroundRepeat={"no-repeat"}
+                    >
+                      <Box
+                        position={"absolute"}
+                        bottom={0}
+                        display={"flex"}
+                        w="full"
+                        flexDirection={"column"}
+                        alignItems={"center"}
+                        justifyContent={"center"}
+                      >
+                        <Box
+                          position={"relative"}
+                          display={"flex"}
+                          maxW={"max-content"}
+                        >
+                          <Box
+                            position={"absolute"}
+                            inset={0}
+                            bg={"black"}
+                            opacity={0.6}
+                          />
+                          <Text
+                            position={"relative"}
+                            px={2}
+                            color={"#F5F5F5"}
+                            fontSize={"2xl"}
+                            fontWeight={"semibold"}
+                          >
+                            {album.title}
+                          </Text>
+                        </Box>
                       </Box>
                     </Box>
-                  </Box>
-                 </GridItem>
+                 </Link>
+                  </GridItem>
                 ))}
               </>
             )}
