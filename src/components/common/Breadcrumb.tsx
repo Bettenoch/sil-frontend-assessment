b7825b614a43973765ba99894d755a7ca463c5c7
Bettenoch <bettenoch254@gmail.com>
2024-12-05 15:43:13 +0300

    FIX: linters errors

--- src/components/common/Breadcrumb.tsx
+++ src/components/common/Breadcrumb.tsx
@@ -1,38 +1,37 @@
+import { Link } from "@tanstack/react-router"
 // src/components/common/Breadcrumb.tsx
-import React from "react";
+import type React from "react"
 import {
-  BreadcrumbCurrentLink,
-  BreadcrumbLink,
-  BreadcrumbRoot,
-} from "../ui/breadcrumb";
-import { Link } from "@tanstack/react-router";
+	BreadcrumbCurrentLink,
+	BreadcrumbLink,
+	BreadcrumbRoot,
+} from "../ui/breadcrumb"
 
 interface BreadcrumbItem {
-  label: string;
-  href?: string;
+	label: string
+	href?: string
 }
 
 interface ReusableBreadcrumbProps {
-  items: BreadcrumbItem[];
+	items: BreadcrumbItem[]
 }
 
 const Breadcrumb: React.FC<ReusableBreadcrumbProps> = ({ items }) => {
-  return (
-    <BreadcrumbRoot>
-      {items.map((item, index) =>
-        item.href ? (
-          <BreadcrumbLink as={Link} key={index} href={item.href}>
-            {item.label}
-          </BreadcrumbLink>
-        ) : (
-          <BreadcrumbCurrentLink key={index}>
-            {item.label}
-          </BreadcrumbCurrentLink>
-        )
-      )}
-    </BreadcrumbRoot>
-  );
-};
+	return (
+		<BreadcrumbRoot>
+			{items.map((item, index) =>
+				item.href ? (
+					<BreadcrumbLink as={Link} key={index} href={item.href}>
+						{item.label}
+					</BreadcrumbLink>
+				) : (
+					<BreadcrumbCurrentLink key={index}>
+						{item.label}
+					</BreadcrumbCurrentLink>
+				),
+			)}
+		</BreadcrumbRoot>
+	)
+}
 
-export default Breadcrumb;
-
+export default Breadcrumb
