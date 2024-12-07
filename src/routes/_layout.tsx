0a8b3e0d76b0cced2198f260bd323a4c4bfd4711
Bettenoch <bettenoch254@gmail.com>
2024-12-07 14:18:29 +0300

    ADD: Toaster to main layout

--- src/routes/_layout.tsx
+++ src/routes/_layout.tsx
@@ -3,6 +3,7 @@ import { Outlet, createFileRoute } from "@tanstack/react-router"
 
 import Footer from "../components/common/Footer"
 import Navbar from "../components/common/Navbar"
+import { Toaster } from "../components/ui/toaster"
 
 export const Route = createFileRoute("/_layout")({
 	component: Layout,
@@ -12,7 +13,7 @@ function Layout() {
 	return (
 		<Flex maxW="large" direction={"column"} h="auto" position="relative">
 			<Navbar />
-
+			<Toaster />
 			<Outlet />
 			<Footer />
 		</Flex>
